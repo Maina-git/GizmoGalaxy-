2c46dfd9333f2c113c9a5c5fb488d8c4ca63eb54
Tornshorts <kawewaalex@gmail.com>
2025-05-20 22:20:30 +0300

    card changes

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -80,11 +80,14 @@ const navItems2:Links2[]=[
 
   return (
     <nav className="bg-slate-801 text-slate-300  shadow-md py-4 text-slate-300 ">
+     
       <div className="container mx-auto flex justify-between items-center px-4">
+      <Link to ={"/"}>
         <div className="flex items-center text-xl font-bold text-blue-500">
           <FaShoppingCart className="mr-2" />
           <span>Gizmo Galaxy</span>
         </div>
+        </Link>
         {NavItmes.map((item) => (
           <div className="hidden md:flex gap-2 text-slate-300   font-medium">
             <Link
